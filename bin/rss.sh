578900d13f2068c08c8bc3cc894c03284e0f54fc
Lucas Holt <luke@foolishgames.com>
2025-04-17 23:56:13 -0400

    more cleanup

--- bin/rss.sh
+++ bin/rss.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/home/mbsd/bin/rss2htmlsum.pl http://www.justjournal.com/users/mbsd/rss /home/mbsd/docs/mbsdblogsum.inc
-/home/mbsd/bin/rss2html.pl http://www.justjournal.com/users/mbsd/rss  /home/mbsd/docs/mbsdblog.inc
+/home/mbsd/bin/rss2htmlsum.pl https://www.justjournal.com/users/mbsd/rss /home/mbsd/docs/mbsdblogsum.inc
+/home/mbsd/bin/rss2html.pl https://www.justjournal.com/users/mbsd/rss  /home/mbsd/docs/mbsdblog.inc
